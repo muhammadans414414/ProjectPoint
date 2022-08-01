7175d75814091ac6a39860172020d656492d1d64
Muhammad ans <muhammad.ans@pitb.gov.pk>
2022-08-01 12:05:54 +0500

    modified permissions in user_projects_controller

--- app/controllers/admins/user_projects_controller.rb
+++ app/controllers/admins/user_projects_controller.rb
@@ -2,6 +2,7 @@ class Admins::UserProjectsController < ApplicationController
     
     def new
         @user_project=UserProject.new
+        authorize! :read, @user_project
     end
     def create
         @user_project=UserProject.new(user_project_params)
@@ -18,6 +19,7 @@ class Admins::UserProjectsController < ApplicationController
     
     def destroy
         @project=UserProject.find(params[:id])
+        authorize! :read, @project
         @project.destroy
         redirect_to admins_projects_path
        
