ac7515ae30447396e0c0a9a8b4d48b9c9da803b7
Muhammad ans <muhammad.ans@pitb.gov.pk>
2022-08-12 14:07:35 +0500

    added application wide tailwind css

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,4 @@
 # uncomment following code for one time only when cloning this app
 # Role.create(name:"admin")
 # Role.create(name:"developer")
-User.create(email:"muhammad.ans@pitb.gov.pk", role_id: Role.where(name:Admin).first.id,password: 12345678, password_confirmation:12345678)
+# User.create(email:"muhammad.ans@pitb.gov.pk", role_id: Role.where(name:Admin).first.id,password: 12345678, password_confirmation:12345678)
